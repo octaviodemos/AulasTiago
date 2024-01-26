adf2577fe693ba4205d206cee2b81681b77b3342
Octávio Demos <octavio_demos@hotmail.com>
2024-01-26 18:06:09 -0300

    sqa

--- ExericioTela/MainWindow.xaml.cs
+++ ExericioTela/MainWindow.xaml.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Data.SqlClient;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Controls;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+
 
 namespace ExercicioTela
 {
@@ -18,9 +20,38 @@ namespace ExercicioTela
         int quantidadeMulheresMenos170 = 0;
         double alturaHomemMaisAlto = 0;
 
+        List<Pessoa> listaPessoas = new List<Pessoa>();
+        
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        public static void InserirPessoa(Pessoa pessoa)
+        {
+            using(SqlConnection conn = new SqlConnection("Data Source=localhost;Initial Catalog=Sexatura;Integrated Security=False;User Id=sa;Password=super990025;"))
+            {
+                conn.Open();
+                SqlCommand command = conn.CreateCommand();
+                string sql = $@"
+INSERT INTO [dbo].[Pessoas]
+           ([Id]
+           ,[Sexo]
+           ,[Altura])
+     VALUES
+           ({pessoa.Id}
+           ,'{pessoa.Sexo}'
+           ,{pessoa.Altura.ToString().Replace(",", ".")}
+";
+                command.CommandText = sql; //define o texto do comando
+                int qtd = command.ExecuteNonQuery(); //retorna linhas afetadas
+                Console.WriteLine("Linhas afetadas:" + qtd);
+            }
+
+
+
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -28,6 +59,7 @@ namespace ExercicioTela
             string sexoSelecionado = string.Empty;
             string alturaDigitada = AlturaText.Text;
             double altura;
+
             if (CheckFeminino.IsChecked == true)
             {
                 sexoSelecionado = "Feminino";
@@ -36,6 +68,12 @@ namespace ExercicioTela
             if (CheckMasculino.IsChecked == true)
             {
                 sexoSelecionado = "Masculino";
+            }
+
+            if (CheckFeminino.IsChecked == false && CheckMasculino.IsChecked == false)
+            {
+                MessageBox.Show("Selecione o sexo da pessoa que está cadastrando");
+                return;
             }
 
             if (string.IsNullOrEmpty(alturaDigitada))
@@ -52,8 +90,6 @@ namespace ExercicioTela
                 return;
             }
 
-            MessageBox.Show("Dados salvos");
-
             if (pessoaatual < 8)
             {
                 Pessoa novaPessoa = new Pessoa
@@ -61,41 +97,42 @@ namespace ExercicioTela
                     Sexo = sexoSelecionado,
                     Altura = altura
                 };
-                novaPessoa.PropertyChanged += NovaPessoa_PropertyChanged;
-                novaPessoa.Altura *= 2;
+
+                listaPessoas.Add(novaPessoa);
+                InserirPessoa(novaPessoa);
+                ParcialListBox.ItemsSource = null;
+                ParcialListBox.ItemsSource = listaPessoas;
                 pessoas[pessoaatual] = novaPessoa;
+
+
             }
 
-            CheckFeminino.IsChecked = false;
-            CheckMasculino.IsChecked = false;
-            AlturaText.Text = string.Empty;
-
-            AlturaText.Focus();
             pessoaatual++;
 
             if (pessoaatual == 8)
             {
                 MostrarResultados();
             }
-        }
 
-        private void NovaPessoa_PropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            MessageBox.Show("Alguem mudou");
+            MessageBox.Show("Dados salvos");
+
+            CheckFeminino.IsChecked = false;
+            CheckMasculino.IsChecked = false;
+            AlturaText.Text = string.Empty;
+
+            AlturaText.Focus();
+
         }
 
         public class Pessoa : INotifyPropertyChanged
         {
-            public event PropertyChangedEventHandler PropertyChanged;
-
             private string _sexo;
             private double _altura;
 
-            public string Sexo { 
-                get
-                {
-                    return _sexo;
-                }
+            public int Id { get; set; }
+            public string Sexo
+            {
+                get { return _sexo; }
                 set
                 {
                     if (_sexo != value)
@@ -103,19 +140,27 @@ namespace ExercicioTela
                         _sexo = value;
                         NotifyPropertyChanged();
                     }
-                } 
-            }
-            public double Altura
-            {
-                get => _altura; 
-                set
-                {
-                    _altura = value;
-                    NotifyPropertyChanged();
                 }
             }
 
-            public void NotifyPropertyChanged([CallerMemberName] string propName = null)
+            public double Altura
+            {
+                get { return _altura; }
+                set
+                {
+                    if (_altura != value)
+                    {
+                        _altura = value;
+                        NotifyPropertyChanged();
+                    }
+                }
+            }
+
+            public string MostrandoPessoas => $"{Sexo}, {Altura}m";
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            protected virtual void NotifyPropertyChanged([CallerMemberName] string propName = "")
             {
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
             }
@@ -125,23 +170,25 @@ namespace ExercicioTela
         {
             foreach (var pessoa in pessoas)
             {
-                if (pessoa.Sexo.ToLower() == "feminino" && pessoa.Altura < 1.70)
+                if (pessoa != null)
                 {
-                    quantidadeMulheresMenos170++;
-                }
-
-                if (pessoa.Sexo.ToLower() == "masculino")
-                {
-                    if (pessoa.Altura > alturaHomemMaisAlto)
+                    if (pessoa.Sexo.ToLower() == "feminino" && pessoa.Altura < 1.70)
                     {
-                        alturaHomemMaisAlto = pessoa.Altura;
+                        quantidadeMulheresMenos170++;
+                    }
+
+                    if (pessoa.Sexo.ToLower() == "masculino")
+                    {
+                        if (pessoa.Altura > alturaHomemMaisAlto)
+                        {
+                            alturaHomemMaisAlto = pessoa.Altura;
+                        }
                     }
                 }
             }
 
             ResultsText.Text = $"Quantidade de mulheres com menos de 1.70m: {quantidadeMulheresMenos170}\nAltura do homem mais alto: {alturaHomemMaisAlto} metros";
         }
-
 
         private void CheckFeminino_Checked(object sender, RoutedEventArgs e)
         {
@@ -160,5 +207,33 @@ namespace ExercicioTela
                 Button_Click(null, null);
             }
         }
+
+        private void ZerarDados()
+        {
+
+            CheckFeminino.IsChecked = false;
+            CheckMasculino.IsChecked = false;
+            AlturaText.Text = string.Empty;
+            listaPessoas.Clear();
+            ParcialListBox.ItemsSource = null;
+
+            quantidadeMulheresMenos170 = 0;
+            alturaHomemMaisAlto = 0;
+
+            pessoas = new Pessoa[8];
+
+            pessoaatual = 0;
+
+            ResultsText.Text = string.Empty;
+        }
+
+        private void Reiniciar(object sender, RoutedEventArgs e)
+        {
+            ZerarDados();
+
+            MessageBox.Show("Reniciado, pode preencher os dados novamente");
+        }
+
     }
+
 }
